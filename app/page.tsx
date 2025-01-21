c0193a84c807f57e5417e5d9ce89f274252c2f39
saveljev27 <141144801+saveljev27@users.noreply.github.com>
2025-01-21 20:19:26 +0200

    first commit and home page scratch

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,66 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="padding-x padding-y max-width">
+      <header>
+        <div className="flex justify-between border-b-2 p-2">
+          <div>English</div>
+          <button className="bg-[#ffbc01] py-1 px-2 rounded-xl mt-2 hover:bg-opacity-60 text-sm">
+            My Profile
+          </button>
+        </div>
+        <nav className="flex justify-between p-2">
+          <div>Logo</div>
+          <div>
+            <ul className="flex *:cursor-pointer *:transition">
+              <li className="p-2 hover:opacity-60">Home</li>
+              <li className="p-2 hover:opacity-60">About</li>
+              <li className="p-2 hover:opacity-60">Contact</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div className="flex gap-2 mt-8">
+          <div className="bg-[#fff7e7] grow rounded-lg shadow-md hover:shadow-lg transition">
+            <div className="my-20 flex flex-col justify-center">
+              <h1 className="text-center">Ready to send?</h1>
+              <button className="bg-[#ffbc01] w-1/4 mx-auto py-2 px-4 rounded-xl mt-2 hover:bg-opacity-60 transition">
+                Start sending
+              </button>
+            </div>
+          </div>
+          <div className="bg-[#FAEAFD] grow rounded-lg shadow-md hover:shadow-lg transition">
+            <div className="my-20 flex flex-col justify-center">
+              <h1 className="text-center">Where is my parcel?</h1>
+              <button className="border border-black w-1/4 mx-auto py-2 px-4 rounded-xl mt-2 hover:border-opacity-60 transition">
+                Start tracking
+              </button>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer>
+        <div className="flex justify-between mt-8">
+          <div>Logo</div>
+          <div>
+            <ul className="flex gap-3">
+              <li>Instagram</li>
+              <li>Facebook</li>
+              <li>Twitter</li>
+            </ul>
+          </div>
+        </div>
+        <div className="border-t-2 mt-8" />
+        <div className="flex justify-between mt-6">
+          <p>Copyright 2025</p>
+          <div>
+            <ul className="flex gap-3">
+              <li>Privacy Policy</li>
+              <li>Terms of Service</li>
+              <li>Cookie Policy</li>
+            </ul>
+          </div>
+        </div>
       </footer>
     </div>
   );
